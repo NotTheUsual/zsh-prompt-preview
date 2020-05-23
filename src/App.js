effa1f883896b9794136b06c0dc26e47cd50ef0b
James Hunter <james.hunter@me.com>
2020-05-23 21:49:06 +0100

    Hook it up to a UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import parsePrompt from './parsePrompt/parsePrompt';
 import './App.css';
 
+const useInput = (initialState) => {
+  const [value, setValue] = useState(initialState);
+  const handleChange = (event) => setValue(event.target.value);
+  return [value, handleChange];
+}
+
 function App() {
+  const [promptConfig, setPromptConfig] = useInput('');
+
+  const handleSubmit = () => {
+    console.log('You don’t need to do that');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+    <main className="main">
+      <h1 className="title">zsh prompt</h1>
+      <form className="form" onSubmit={handleSubmit}>
+        <label for="input" className="label">
+          Enter config string
+        </label>
+        <span className="input-context">PS1=“</span>
+        <input type="text" className="input" value={promptConfig} onChange={setPromptConfig} />
+        <span className="input-context">”</span>
+      </form>
+      <code className="terminal">
+        <p className="terminal-text">
+          {(promptConfig)
+            ? parsePrompt(promptConfig)
+            : '$ '
+          }
+          cd somewhere
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </code>
+    </main>
   );
 }
 
